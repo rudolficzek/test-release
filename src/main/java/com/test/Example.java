8aa61ceac83e56a2716bb6a61ce5d208980364c9
Katarzyna Fidos <katarzynafidos@currencyfair.com>
2016-04-19 09:51:19 +0100

    cm

---
+++ src/main/java/com/test/Example.java
@@ -0,0 +1,10 @@
+package com.test;
+
+/**
+ * Created by kfidos on 19/04/2016.
+ */
+public class Example {
+    public static void main(String[] args) {
+        System.out.println("Mr bad example");
+    }
+}
