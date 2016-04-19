be8e89ce5f006f8e29e29f90a12cfa33fab2bf00
Katarzyna Fidos <katarzynafidos@currencyfair.com>
2016-04-19 14:19:41 +0100

    add test

---
+++ src/test/java/com/test/ExampleTest.java
@@ -0,0 +1,18 @@
+package com.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by kfidos on 19/04/2016.
+ */
+
+public class ExampleTest {
+
+   @Test
+   public void testExample(){
+       Assert.assertFalse(false);
+       System.out.println("test");
+   }
+
+}
